c005eb8af1221dbf8030388ca022b335e0bfd121
CoderUdSingh <uddeshya.professional07@gmail.com>
2024-01-02 14:36:56 +0530

    Added DB connection, Schema and Sign In , Sign Up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,13 @@
 exports.profile = (req, res) => {
   return res.render("users", { title: "Users" });
 };
+
+// Rendering Sign Up Page
+exports.signUp = (req, res) => {
+  return res.render("user_sign_up", { title: "Users-Sign-Up" });
+};
+
+// Rendering Sign In Page
+exports.signIn = (req, res) => {
+  return res.render("user_sign_in", { title: "Users-Sign-In" });
+};
--- index.js
+++ index.js
@@ -2,13 +2,51 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const path = require("path");
+const mongoose = require("mongoose");
+
+////////////////////////////////////////////Setting up the DB//////////////////////////////////////////////////
+
+const User = require("./models/user_Schema");
+
+///////////////////////////////////////////Including Env File///////////////////////////////////////////////////
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+//////////////////////////////////// Establishing DB Connection Through Mongoose//////////////////////////////////
+
+const dbLocal = process.env.DATABASE_LOCAL;
+// const URI = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
+
+// mongoose
+//   .connect(URI)
+//   .then(() => console.log("Dadabase Connection is Successful on Atlas"))
+//   .catch((err) => console.error(`Error in connecting the DB ${err}`));
+
+mongoose
+  .connect(dbLocal)
+  .then(() => console.log("Dadabase Connection is Successful"))
+  .catch((err) => console.error(`Error in connecting the DB ${err}`));
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.log(err);
+});
+
+////////////////////////////////////Including static files////////////////////////////////////
 
 app.use(express.static("./assets"));
 app.use(require("express-ejs-layouts"));
 
+///////////////////////////////// Setting up the Express Layouts///////////////////////////////
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
+//////////////////////////////// Setting up the view engine properties/////////////////////////
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 app.use("/", require("./routes"));
