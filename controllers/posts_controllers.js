4b22ce2b0f7901a1362cda87987a4bc8f2411a4c
CoderUdSingh <uddeshya.professional07@gmail.com>
2024-01-09 02:32:36 +0530

    Added functionality for Deleting posts alongwith the comments associated

--- controllers/posts_controllers.js
+++ controllers/posts_controllers.js
@@ -1,6 +1,24 @@
 const Post = require("../models/Post_Schema");
+const Comment = require("../models/Comment_Schema");
 
 exports.createPost = (req, res) => {
   Post.create({ content: req.body.content, user: req.user._id });
   return res.redirect("back");
+};
+
+exports.destroy = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+      if (post.user == req.user.id) {
+        await Post.deleteOne({ user: req.user.id });
+        await Comment.deleteMany({ post: req.params.id });
+      }
+
+      return res.redirect("back");
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  res.redirect("back");
 };
--- routes/posts_route.js
+++ routes/posts_route.js
@@ -2,6 +2,12 @@ const express = require("express");
 const postController = require("../controllers/posts_controllers");
 const router = express.Router();
 const passport = require("passport");
+
 router.post("/create", passport.checkAuthentication, postController.createPost);
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  postController.destroy
+);
 
 module.exports = router;
